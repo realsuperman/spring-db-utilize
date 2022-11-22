6f747ab8870948693b1f776d69dffb73367a7bb5
최성훈 <seonghun7304@naver.com>
2022-11-22 19:38:11 +0900

    jdbc template 기초 관련 공부함

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -9,7 +9,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 
-@Import(MemoryConfig.class)
+//@Import(MemoryConfig.class)
+@Import(JdbcTemplateV1Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
@@ -17,8 +18,9 @@ public class ItemServiceApplication {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
-	@Bean
-	@Profile("local")
+	@Bean // 빈 등록
+	@Profile("local") // application.properties의 속성 spring.profiles.active가 해당 조건을 만족하는 경우 수행한다는 의미이다
+	// 스프링은 로딩 시점에 spring.profiles.active의 값을 읽어서 프로필 값으로 설정한다
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
 	}
--- src/main/java/hello/itemservice/TestDataInit.java
+++ src/main/java/hello/itemservice/TestDataInit.java
@@ -16,8 +16,8 @@ public class TestDataInit {
     /**
      * 확인용 초기 데이터 추가
      */
-    @EventListener(ApplicationReadyEvent.class)
-    public void initData() {
+    @EventListener(ApplicationReadyEvent.class) // 어플리케이션이 준비 되었을 때(스프링이 모든 준비를 끝마침) 아래를 실행한다
+    public void initData() { // 서버 시작시 데이터 추가
         log.info("test data init");
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
